f9ea6b55c26b754ad53db8b461a3a7f8e1783ce1
HanZhuoii <e.hanzhuo@gmail.com>
2020-11-17 17:57:17 +0800

    V1.0 全站基础信息实现

--- frame/mail.py
+++ frame/mail.py
@@ -2,6 +2,7 @@ import smtplib
 from time import sleep
 from email.mime.text import MIMEText
 from frame.SpiderFrame import logger
+import config
 
 
 def send_mail(content):
@@ -13,7 +14,7 @@ def send_mail(content):
         # 163用户名
         mail_user = 'hanzhuoii@163.com'
         # 密码(部分邮箱为授权码)
-        mail_pass = 'RICFGHQXKBUPWBQN'
+        mail_pass = config.MAIL_PASSWD
         # 邮件发送方邮箱地址
         sender = 'hanzhuoii@163.com'
         # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
@@ -44,6 +45,7 @@ def send_mail(content):
             logger.info("Successfully send e-mail to: {0} - [{1}]".format(receivers[0], content))
             return
         except Exception:
-            logger.error("Fail to send Email, retry {0}times... Message: {1} - [{2}]".format(i, receivers[0], content), exc_info=True)
+            logger.error("Fail to send Email, retry {0}times... Message: {1} - [{2}]".format(i, receivers[0], content),
+                         exc_info=True)
             i += 1
             sleep(10)
