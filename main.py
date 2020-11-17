57b07d4ea52b888228c8fe3530b46be4fb81af8b
HanZhuoii <e.hanzhuo@gmail.com>
2020-11-17 17:44:24 +0800

    V1.0 全站基础信息实现

--- main.py
+++ main.py
@@ -148,13 +148,13 @@ def ProcessError():
             int(key)
             url = redis.get(key).decode("utf-8").split("/")
             if url[5] == "answers":
-                redis.rpush(config.ANSWER_ID_SET, url[5])
+                redis.rpush("list_"+config.ANSWER_ID_SET, url[5])
                 redis.delete(key)
             elif url[5] == "questions":
-                redis.rpush(config.QUESTION_ID_SET, url[5])
+                redis.rpush("list_"+config.ANSWER_ID_SET, url[5])
                 redis.delete(key)
             elif url[5] == "topics":
-                redis.rpush(config.TOPIC_ID_SET, url[5])
+                redis.rpush("list_"+config.ANSWER_ID_SET, url[5])
                 redis.delete(key)
         except:
             pass
